41ce08d6a6f4de9b69e837639e3bc3a03de383f5
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-10-31 10:50:06 +0100

    automigrate modificado

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -1,19 +1,27 @@
+/* global process */
+
 'use strict';
 
 module.exports = function(app) {
     //data sources
     var mysqlDs = app.dataSources.mysqlDs;
+    
+    if(process.env.AUTOMIGRATE){
 
-    mysqlDs.automigrate(null, function (err) {
-        if (err) console.log(err);
-        console.log('> Models migrated to tables');
-        
-        app.loadFixtures()
-        .then(function() {
-          console.log('Done!');
-        })
-        .catch(function(err) {
-          console.log('Errors:', err);
+        mysqlDs.automigrate(null, function (err) {
+            if (err) console.log(err);
+            console.log('> Models migrated to tables');
+
+            app.loadFixtures()
+            .then(function() {
+              console.log('Done!');
+            })
+            .catch(function(err) {
+              console.log('Errors:', err);
+            });
         });
-    });
+    
+   }
+        
+    
 }
