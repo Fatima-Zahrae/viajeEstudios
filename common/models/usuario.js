fdff80a9cf8cc417a7ee61b78100562af9579de3
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-11-07 10:06:48 +0100

    la vista y la verificacion creados

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,34 @@
 'use strict';
+var config = require('../../server/config.json');
+var path = require('path'); 
 
 module.exports = function(Usuario) {
+    
+     //send verification email after registration
+  Usuario.afterRemote('create', function(context, usuario, next) {
+    var options = {
+      type: 'email',
+      to: usuario.email,
+      from: 'noreply@loopback.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      usuario: usuario
+    };
+
+    usuario.verify(options, function(err, response) {
+      if (err) {
+        Usuario.deleteById(usuario.id);
+        return next(err);
+      }
+      context.res.render('response', {
+        title: 'Signed up successfully',
+        content: 'Please check your email and click on the verification link ' +
+            'before logging in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+});
 
 };
--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,7 @@
 
 var dsConfig = require('../datasources.local.js');
 var path = require('path');
+//var usuario = app.models.Usuario;
 
 module.exports = function(app) {
     var usuario = app.models.Usuario;
@@ -16,8 +17,61 @@ module.exports = function(app) {
     });
 });
 
+//verified
+  app.get('/verified', function(req, res) {
+    res.render('verified');
+});
+
+//log a user in
+  app.post('/login', function(req, res) {
+    usuario.login({
+      email: req.body.EMAIL_USER,
+      password: req.body.EMAIL_PASSWORD
+    }, 'user', function(err, token) {
+      if (err) {
+        if(err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED'){
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/users/'+ err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+      res.render('home', {
+        email: req.body.EMAIL_USER,
+        accessToken: token.id,
+        redirectUrl: '/api/users/change-password?access_token=' + token.id
+      });
+    });
+});
+
+  //log a user out
+  app.get('/logout', function(req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401);
+    usuario.logout(req.accessToken.id, function(err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
+});
+
+
   app.use(router);
 };
+
+
+
+
 
 
 
